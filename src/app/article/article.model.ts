05306c601115affc3c821d5ed3619075300d28fd
Dana Palazzo <dana@fretless.com>
2018-07-19 14:51:10 -0400

    Render multiple articles to page

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -4,7 +4,7 @@ export class Article {
     votes: number;
 
     constructor(title: string, link: string, votes?: number) {
-        this.title = link;
+        this.title = title;
         this.link = link;
         this.votes = votes || 0;
     }
