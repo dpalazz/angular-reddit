1cdd91723e90bab3ec002ea2988a43da9f740b00
Dana Palazzo <dana@fretless.com>
2018-07-20 10:54:37 -0400

    Sort articles by vote score

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -13,16 +13,16 @@ export class Article {
         this.votes += 1;
       }
     
-      voteDown(): void {
-        this.votes -= 1;
-      }
+    voteDown(): void {
+    this.votes -= 1;
+    }
 
-      domain(): string {
-          try {
-            const domainAndPath: string = this.link.split('//')[1];
-            return domainAndPath.split('/')[0];
-          } catch (err) {
-              return null;
-          }
-      }
+    domain(): string {
+        try {
+        const domainAndPath: string = this.link.split('//')[1];
+        return domainAndPath.split('/')[0];
+        } catch (err) {
+            return null;
+        }
+    }
 }
